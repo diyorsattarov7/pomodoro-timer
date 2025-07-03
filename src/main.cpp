651abe920466aaa12f23a225dc3c10f74c502b41
Diyor Sattarov <diyorsattarov@outlook.com>
2025-07-03 15:35:37 -0500

    feat: add timer functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,86 @@
-#include <QTWidgets/QPushButton>
+#include <QTimer>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QVBoxLayout>
 
 class PomodoroTimer : public QMainWindow
 {
+    Q_OBJECT
 public:
     PomodoroTimer(QWidget *parent = nullptr) : QMainWindow(parent)
     {
         setWindowTitle("Pomodoro Timer");
         setFixedSize(450, 400);
         setUpUI();
+        setupTimer();
+        resetTimer();
     }
 
+private slots:
+    void toggleTimer()
+    {
+        if (timer->isActive())
+        {
+            timer->stop();
+            startStopBtn->setText("Start");
+        }
+        else
+        {
+            timer->start(1000);
+            startStopBtn->setText("Pause");
+        }
+    }
+
+    void updateDisplay()
+    {
+        remainingTime--;
+
+        int minutes = remainingTime / 60;
+        int seconds = remainingTime % 60;
+
+        timerLabel->setText(QString("%1:%2")
+                                .arg(minutes, 2, 10, QChar('0'))
+                                .arg(seconds, 2, 10, QChar('0')));
+
+        if (remainingTime <= 0)
+        {
+            timer->stop();
+            onTimerFinished();
+        }
+    }
+
+    void onTimerFinished()
+    {
+        isWorkSession = !isWorkSession;
+        statusLabel->setText(isWorkSession ? "Work Session" : "Break Time");
+        statusLabel->setStyleSheet(
+            isWorkSession
+                ? "color: #e74c3c; font-weight: bold; font-size: 18px;"
+                : "color: #27ae60; font-weight: bold; font-size: 18px;");
+
+        resetTimer();
+        startStopBtn->setText("Start");
+    }
+
+    void resetTimer()
+    {
+        timer->stop();
+        remainingTime = isWorkSession ? workDuration * 60 : breakDuration * 60;
+
+        int minutes = remainingTime / 60;
+        int seconds = remainingTime % 60;
+
+        timerLabel->setText(QString("%1:%2")
+                                .arg(minutes, 2, 10, QChar('0'))
+                                .arg(seconds, 2, 10, QChar('0')));
+
+        startStopBtn->setText("Start");
+    }
+
+private:
     void setUpUI()
     {
         QWidget *centralWidget = new QWidget;
@@ -58,6 +125,9 @@ public:
                                     "background-color: #1e8449; "
                                     "} ");
 
+        connect(startStopBtn, &QPushButton::clicked, this,
+                &PomodoroTimer::toggleTimer);
+
         QPushButton *resetBtn = new QPushButton("Reset");
         resetBtn->setFixedHeight(50);
         resetBtn->setStyleSheet("QPushButton { "
@@ -74,16 +144,31 @@ public:
                                 "QPushButton:pressed { "
                                 "background-color: #a93226; "
                                 "}");
+
+        connect(resetBtn, &QPushButton::clicked, this,
+                &PomodoroTimer::resetTimer);
+
         buttonLayout->addWidget(startStopBtn);
         buttonLayout->addWidget(resetBtn);
         mainLayout->addLayout(buttonLayout);
         mainLayout->addStretch();
     }
 
-private:
+    void setupTimer()
+    {
+        timer = new QTimer(this);
+        connect(timer, &QTimer::timeout, this, &PomodoroTimer::updateDisplay);
+    }
+
+    QTimer *timer;
     QLabel *timerLabel;
     QLabel *statusLabel;
     QPushButton *startStopBtn;
+
+    int workDuration = 25;
+    int breakDuration = 5;
+    int remainingTime = 0;
+    bool isWorkSession = true;
 };
 
 int main(int argc, char **argv)
@@ -95,3 +180,5 @@ int main(int argc, char **argv)
 
     return app.exec();
 }
+
+#include "main.moc"
