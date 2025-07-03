05b9f72534ae75979bc393ff3b45ccd29337cff1
Diyor Sattarov <diyorsattarov@outlook.com>
2025-07-03 15:01:03 -0500

    feat: add basic layout & timer display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QVBoxLayout>
 
 class PomodoroTimer : public QMainWindow
 {
@@ -8,7 +10,32 @@ public:
     {
         setWindowTitle("Pomodoro Timer");
         setFixedSize(450, 400);
+        setUpUI();
     }
+
+    void setUpUI()
+    {
+        QWidget *centralWidget = new QWidget;
+        setCentralWidget(centralWidget);
+
+        QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+        mainLayout->setSpacing(15);
+        mainLayout->setContentsMargins(20, 20, 20, 20);
+
+        timerLabel = new QLabel("25:00");
+        timerLabel->setAlignment(Qt::AlignCenter);
+        timerLabel->setStyleSheet("font-size: 72px; "
+                                  "font-weight: bold; "
+                                  "color: #2c3e50; "
+                                  "background-color: #ecf0f1; "
+                                  "border-radius: 20px; "
+                                  "padding: 40px; ");
+        mainLayout->addWidget(timerLabel);
+        mainLayout->addStretch();
+    }
+
+private:
+    QLabel *timerLabel;
 };
 
 int main(int argc, char **argv)
